6c495b9efa377cd6d554264f81ae0279b843ef25
данечка ыыыы <akkhazriel@gmail.com>
2023-06-12 15:49:47 +0300

    update

--- js/script.js
+++ js/script.js
@@ -74,3 +74,62 @@ $(document).ready(function () {
     e.preventDefault();
   });
 });
+
+// PRICE LINKS ACTIVE
+$('.price__link').click(function () {
+  $('.price__link').removeClass('active');
+  $(this).addClass('active');
+})
+
+// PRICE ITEM ACTIVE
+
+const priceItems = document.querySelectorAll('.price__item'); // получаем все блоки price__item
+
+for (let i = 0; i < priceItems.length; i++) {
+  const priceItem = priceItems[i];
+
+  // навешиваем обработчик события на каждый блок price__item
+  priceItem.addEventListener('click', function() {
+    // убираем класс active со всех блоков price__item
+    for (let j = 0; j < priceItems.length; j++) {
+      priceItems[j].classList.remove('active');
+      priceItems[j].querySelector('.price__item-btn').classList.remove('active');
+    }
+
+    // добавляем класс active на текущий блок price__item и на его дочерний элемент .price__item-btn
+    priceItem.classList.add('active');
+    priceItem.querySelector('.price__item-btn').classList.add('active');
+  });
+}
+
+// PRICE QUESTIONS
+const questionsItems = document.querySelectorAll('.price__questions-item');
+
+for (let i = 0; i < questionsItems.length; i++) {
+  const questionsItem = questionsItems[i];
+
+  questionsItem.addEventListener('click', function() {
+    for (let j = 0; j < questionsItems.length; j++) {
+      const questionsDesc = questionsItems[j].querySelector('.price__questions-desc');
+      if (questionsDesc) {
+        questionsDesc.classList.remove('active');
+      }
+      
+      const questionsImg = questionsItems[j].querySelector('img');
+      if (questionsImg) {
+        questionsImg.src = 'img/price/arrow-close.png';
+      }
+    }
+
+    const activeQuestionsDesc = questionsItem.querySelector('.price__questions-desc');
+    if (activeQuestionsDesc) {
+      activeQuestionsDesc.classList.add('active');
+    }
+
+    const activeQuestionsImg = questionsItem.querySelector('img');
+    if (activeQuestionsImg) {
+      activeQuestionsImg.src = 'img/price/arrow-open.png';
+    }
+
+  });
+}
